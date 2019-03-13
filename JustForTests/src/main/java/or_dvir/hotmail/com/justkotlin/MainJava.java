4744c203629a419d00fd6d3934b6d1784b958053
Or Dvir <or_dvir@hotmail.com>
2019-03-13 13:16:50 +0200

    added java class

---
+++ JustForTests/src/main/java/or_dvir/hotmail/com/justkotlin/MainJava.java
@@ -0,0 +1,16 @@
+package or_dvir.hotmail.com.justkotlin;
+
+public class MainJava
+{
+    public static void main(String[] args)
+    {
+        int[] data = {1,2,3};
+        System.out.println(solution(data));
+    }
+
+    private static int solution(int[] a)
+    {
+        //enter java code here
+        return 0; //just for compiler
+    }
+}
---
+++ JustForTests/src/main/java/or_dvir/hotmail/com/justkotlin/MainKotlin.kt
@@ -0,0 +1,20 @@
+package or_dvir.hotmail.com.justkotlin
+
+class MainKotlin
+{
+    companion object
+    {
+        @JvmStatic
+        fun main(args: Array<String>)
+        {
+            println(solution(intArrayOf(-1, -3)))
+        }
+
+        @Suppress("MemberVisibilityCanBePrivate")
+        fun solution(a: IntArray): Int
+        {
+            //enter kotlin code here
+            return 0 //just for compiler
+        }
+    }
+}
